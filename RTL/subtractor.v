17a9c0e8483c0b9d7ba8008dcc4842d627d44858
Khang <24520727@gm.uit.edu.vn>
2026-04-03 14:26:28 +0700

    Bo tru

---
+++ RTL/subtractor.v
@@ -0,0 +1,10 @@
+module subtractor(
+    parameter N = 16
+)
+(
+    input wire signed [N-1:0] A,
+    input wire signed [N-1:0] B,
+    input wire signed [N-1:0] C
+)
+    assign C = A - B;
+endmodule
