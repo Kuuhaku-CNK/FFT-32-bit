c6e6a151f8b532a112c8f22c98e2689b2eb49db5
ltmhuy <lethienminhhuy01@gmail.com>
2026-04-12 20:41:09 +0700

    Update Control_unit.v

--- main/Control_unit.v
+++ main/Control_unit.v
@@ -1,32 +1,75 @@
-module Control_unit (
-    input clk, rst,valid, 
-    input [4:0] addr_a_in,addr_b_in,  
+`timescale 1ns / 1ps
+module Control_Unit (
+    input clk, rst, valid_in, done_in,        
+    input wire [4:0] addr_a_agu, addr_b_agu,
     
-    output reg [4:0] addr_a_out, addr_b_out,
-    output reg we
+    output reg ena_agu,        
+    output wire sel, we,            
+    output wire [4:0] addr_a_w, addr_b_w
 );
-    reg [4:0] a0,b0;
-    reg we0;
-    always @(posedge clk)
-    begin
-        if (rst) 
-        begin
-            a0 <= 0;
-            b0 <= 0;
-            addr_a_out <= 0;
-            addr_b_out <= 0;
-	    we0 <= 0;
-	    we <= 0;
-        end
-        else 
-        begin
-          a0 <= addr_a_in;
-          b0 <= addr_b_in;
-          we0 <= valid;
-          
-          addr_a_out <= a0;
-          addr_b_out <= b0;
-          we <= we0;
+    localparam S_IDLE_LOAD = 2'd0;
+    localparam S_CALC      = 2'd1;
+    localparam S_DONE      = 2'd2;
+
+    reg [1:0] state, next_state;
+    reg [5:0] load_counter;    
+    reg [4:0] delay_addr_a [0:2]; 
+    reg [4:0] delay_addr_b [0:2];
+    reg       delay_valid  [0:2];
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            state <= S_IDLE_LOAD;
+            load_counter <= 5'd0;
+        end 
+        else begin
+            state <= next_state;
+            if (state == S_IDLE_LOAD) begin
+                load_counter <= load_counter + 2;
+            end
         end
     end
+    always @(*) begin
+        next_state = state;
+        ena_agu = 1'b0;
+        case (state)
+            S_IDLE_LOAD: begin
+                if (load_counter >= 6'd30)
+                    next_state = S_CALC;  
+            end
+
+            S_CALC: begin
+                ena_agu = 1'b1;           
+                if (done_in)
+                    next_state = S_DONE;  
+            end
+            S_DONE:;
+        endcase
+    end
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            delay_addr_a[0] <= 5'd0; delay_addr_a[1] <= 5'd0; delay_addr_a[2] <= 5'd0;
+            delay_addr_b[0] <= 5'd0; delay_addr_b[1] <= 5'd0; delay_addr_b[2] <= 5'd0;
+            delay_valid[0]  <= 1'b0; delay_valid[1]  <= 1'b0; delay_valid[2]  <= 1'b0;
+        end 
+        else begin
+            delay_addr_a[0] <= addr_a_agu;
+            delay_addr_b[0] <= addr_b_agu;
+            delay_valid[0]  <= valid_in;
+
+            delay_addr_a[1] <= delay_addr_a[0];
+            delay_addr_b[1] <= delay_addr_b[0];
+            delay_valid[1]  <= delay_valid[0];
+
+            delay_addr_a[2] <= delay_addr_a[1];
+            delay_addr_b[2] <= delay_addr_b[1];
+            delay_valid[2]  <= delay_valid[1];
+        end
+    end
+    assign sel = (state == S_CALC || state == S_DONE) ? 1'b1 : 1'b0;
+    assign we = (sel == 1'b0 ) ? 1'b1 : (delay_valid[2]);
+    assign addr_a_w = (sel == 1'b0) ? load_counter : delay_addr_a[2];
+    assign addr_b_w = (sel == 1'b0) ? load_counter + 1 : delay_addr_b[2];
+
 endmodule
