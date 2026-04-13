1086599afdfbb15dd6f7e864d1a5445abebb6fb9
ltmhuy <lethienminhhuy01@gmail.com>
2026-04-13 11:23:20 +0700

    Update AGU.v

--- main/AGU.v
+++ main/AGU.v
@@ -1,13 +1,16 @@
+`timescale 1ns / 1ps
+
 module AGU (
     input clk, rst, enable, 
     output [4:0] addr_a,addr_b,  
-    output [3:0] addr_w,   
+    output [3:0] addr_w,
+    output valid,   
     output reg done     
 );
   reg [2:0] stage;  
   reg [5:0] i,j;
   wire [5:0] len;
-  
+  assign valid = enable & ~done;
   assign len = 6'b000001 << stage;
   assign addr_a = i + j;
   assign addr_b = i + j + len;
